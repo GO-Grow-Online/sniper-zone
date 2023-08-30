c1eeffb59deb5d76c43c91d1e5f1cb00cfdcb52f
GO-Grow-Online <julien.growonline@gmail.com>
2023-08-30 20:32:09 +0200

    tweak

--- send_mail.php
+++ send_mail.php
@@ -6,54 +6,43 @@ require_once 'assets/PHPMailer-master/src/SMTP.php';
 
 // Vérification du formulaire soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $data = array();
-    $response = array();
+    if (isset($_FILES['video'])) {
+        $video = $_FILES['video'];
+        $response = array();
+        $mail = new PHPMailer();
 
-    /*
-    // Get every fields values
-    foreach ($_POST as $name => $value) {
-        $data[$name] = $value;
-    }
-    */
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPSecure = 'ssl'; 
+            $mail->Port = 465;
+            $mail->SMTPAuth = true;
+            $mail->Username = 'julien.growonline@gmail.com';
+            $mail->Password = 'dbiqnrvmmsqcrira';
 
+            // Sender & receiver
+            $mail->setFrom('julien.growonline@gmail.com', 'Sniper zone');
+            $mail->addAddress('julien19991227@gmail.com', 'Client de Sniper zone');
 
-    /*
-    // Add custom HTML template for mail and replace "{ field key }" with the form values
-    $body = file_get_contents('assets/mail-tpl.html');
+            // Mail content
+            $mail->Subject = 'Video';
+            $mail->Body = 'message';
+            
+            // Ajout de la pièce jointe (la vidéo)
+            $mail->addAttachment($video['tmp_name'], $video['name']);
 
-    if(isset($data)){
-        foreach($data as $key => $value){
-            $body = str_replace('{'.strtoupper($key).'}', $value, $body);
+            if ($mail->send()) {
+                $response['success'] = true;
+            } else {
+                $response['error'] =  $mail->ErrorInfo;
+            }
+        } catch (Exception $error) {
+            $response['error'] =  $error->getMessage();
         }
-    }*/
-
-    $mail = new PHPMailer\PHPMailer\PHPMailer();
-
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPSecure = 'ssl'; 
-    $mail->Port = 465;
-    $mail->SMTPAuth = true;
-    $mail->Username = 'julien.growonline@gmail.com';
-    $mail->Password = 'dbiqnrvmmsqcrira';
-
-    // Sender & receiver
-    $mail->setFrom('julien.growonline@gmail.com', 'Sniper zone');
-    $mail->addAddress('julien19991227@gmail.com', 'Client de Sniper zone');
-
-    // Mail content
-    $mail->Subject = 'Video';
-    $mail->Body = 'message';
-
-    if ($mail->send()) {
-        $response['success'] = true;
-    } else {
-        $response['error'] =  $mail->ErrorInfo;
+        
+        header('Content-Type: application/json');
+        echo json_encode($response);
     }
-    
-    header('Content-Type: application/json');
-    echo json_encode($response);
 }
 
 ?>
