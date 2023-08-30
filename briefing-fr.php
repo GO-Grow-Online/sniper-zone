4a9bd0f225ade9d10d3d1ded256d3571bb63bdfb
GO-Grow-Online <julien.growonline@gmail.com>
2023-08-30 18:48:52 +0200

    tweak

--- briefing-fr.php
+++ briefing-fr.php
@@ -1,5 +1,5 @@
 <?php
-$filename = 'assets/medias/video/briefing-fr.mp4';
+$filename = '/assets/medias/video/briefing-fr.mp4';
 $expiry = 60 * 60 * 24 * 30; // 1 month cache
 
 header("Cache-Control: max-age=$expiry");
