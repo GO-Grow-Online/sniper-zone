d001003b950f5d62f55e1592a25101ddc0bbbde2
GO-Grow-Online <julien.growonline@gmail.com>
2024-01-09 18:05:39 +0100

    tweaks

--- assets/app.js
+++ assets/app.js
@@ -154,7 +154,7 @@ jQuery(function($) {
 
             var videoPath = '../assets/medias/video/briefing-' + lang + '.mp4';
 
-            $('#video')[0].src = videoPath;
+            $('#video-' + lang)[0].src = videoPath;
 
             
             popup("brief-begin", delay);
