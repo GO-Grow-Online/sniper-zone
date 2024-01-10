e15750a1dfc1b144e0540b59d34983d6e4b6ff78
GO-Grow-Online <julien.growonline@gmail.com>
2024-01-10 13:58:55 +0100

    tweak

--- service-worker.js
+++ service-worker.js
@@ -44,22 +44,16 @@ const urlsToCache = [
 ];
 
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache)
-                .then(() => console.log('Service Worker: Installation successful'))
-                .catch((error) => console.error('Service Worker: Installation failed', error));
-        })
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.match(e.request).then((response) => {
-                return response || fetch(e.request);
-            });
-        })
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
 });
