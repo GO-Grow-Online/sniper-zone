a0ab968b2a977240309c1d28e47c4a49d95b0063
GO-Grow-Online <julien.growonline@gmail.com>
2024-01-10 11:43:46 +0100

    tweaked site title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sniperzone testing</title>
+    <title>Sniperzone briefing</title>
     <script src="assets/jquery.js"></script>
     <script src="assets/app.js"></script>
     <link rel="stylesheet" href="style.css">
